ff90723eb1ac12c7f524e1c3c0a657c9f2eeb056
Megagig <megagigsoftwaresolutions.com>
2024-06-21 17:06:43 +0100

    Install tailwind Css to the Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Shopmaster eCommerce shop</h1>
+    <div className="text-3xl font-bold underline bg-red-700">
+      <h1 className="bg-red-700">Shopmaster eCommerce shop</h1>
     </div>
   );
 }
